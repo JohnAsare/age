5c841f74474f2fcfd65ed90217ff39edccadd680
JohnAsare <johnasare7@gmail.com>
2018-09-04 13:34:52 -0400

    "Created the directory and the file"

--- Age.py
+++ Age.py
@@ -1,1 +1,11 @@
-""
+# Author: John Asare
+# Date: 09/4/2018
+
+""" Creating a code to ask the user for their age"""
+
+user: input("What is your name: ")
+print("Hello %d", user)
+
+#age = int(input("what is age? "))
+
+
